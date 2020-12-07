3f313ce837c628f411226527f3374c9f7c93a5ce
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-07 23:59:04 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #stl (Wegare)
 wget -O ~/badvpn.ipk "https://github.com/wegare123/stl-tunnel/blob/main/badvpn_1.999.130-1_aarch64_cortex-a53.ipk?raw=true"
-wget -O /etc/redsocks.conf "https://raw.githubusercontent.com/wegare123/stl-tunnel/main/redsocks.conf"
+wget -O /usr/bin/xderm "https://raw.githubusercontent.com/wegare123/xderm-badvpn-tun2socks/main/xderm"
 opkg update && opkg install badvpn.ipk
 chmod +x /usr/bin/xderm
 rm -r ~/badvpn.ipk
