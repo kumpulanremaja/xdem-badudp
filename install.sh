0350f1499afd3d16d6e57d2c4e9ca1363cd7e884
wegare123 <56117745+wegare123@users.noreply.github.com>
2020-12-08 18:26:32 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#stl (Wegare)
+#mod by wegare
 wget -O ~/badvpn.ipk "https://github.com/wegare123/stl-tunnel/blob/main/badvpn_1.999.130-1_aarch64_cortex-a53.ipk?raw=true"
 wget -O /usr/bin/xderm "https://raw.githubusercontent.com/wegare123/xderm-badvpn-tun2socks/main/xderm"
 opkg update && opkg install ip-full && opkg install badvpn.ipk
